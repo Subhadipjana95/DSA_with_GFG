78025b7c24bf22cbf139f2340597b8339f71db17
Subhadip Jana <codesubhadip95@gmail.com>
2026-06-14 01:37:19 +0530

    Sync LeetCode - Weighted Word Mapping (Easy)

---
+++ LeetCode/Easy/3838_WeightedWordMapping.java
@@ -0,0 +1,23 @@
+/**
+ * Problem Link : https://leetcode.com/problems/weighted-word-mapping/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        int w = words.length;
+        StringBuilder ans = new StringBuilder();
+        for(int i=0; i<w; i++){
+            int weight = 0;
+            for(int j=0; j<words[i].length(); j++){
+                char currChar = words[i].charAt(j);
+                int currWeight = (int)(currChar - 'a');
+                weight += weights[currWeight];
+            }
+           int reverseIndex = 25 - weight %26;
+           ans.append((char)(reverseIndex + 'a'));
+        }
+        return ans.toString();
+    }
+}
